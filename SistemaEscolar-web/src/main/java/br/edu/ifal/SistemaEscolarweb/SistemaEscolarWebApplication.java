2894637f1ae1c812e2d15dd3b74546bf36fcfa10
rodrigo <rodrigocr7santista@gmail.com>
2018-04-20 09:43:13 -0300

    Criacao do projeto SistemaEscolar-web

---
+++ SistemaEscolar-web/src/main/java/br/edu/ifal/SistemaEscolarweb/SistemaEscolarWebApplication.java
@@ -0,0 +1,12 @@
+package br.edu.ifal.SistemaEscolarweb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SistemaEscolarWebApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SistemaEscolarWebApplication.class, args);
+	}
+}
