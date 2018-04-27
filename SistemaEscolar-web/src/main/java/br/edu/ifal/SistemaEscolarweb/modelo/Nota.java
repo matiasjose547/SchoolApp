25aed1f39ff81ff5c8552b9b2a1d2c86afe973cd
didilmonteir <didilmonteiro70@gmail.com>
2018-04-27 01:29:37 -0300

    Na Classe nota, exclui os atributos alunos e disciplinas e seus métodos

--- SistemaEscolar-web/src/main/java/br/edu/ifal/SistemaEscolarweb/modelo/Nota.java
+++ SistemaEscolar-web/src/main/java/br/edu/ifal/SistemaEscolarweb/modelo/Nota.java
@@ -11,25 +11,20 @@ import org.hibernate.annotations.GenericGenerator;
 
 @Entity
 @Table(name = "nota")
-
 public class Nota {
+
 	@Id
 	@GeneratedValue(generator = "inc")
 	@GenericGenerator(name = "inc", strategy = "increment")
 	private Integer id;
-	
-	@ManyToOne
-	private Aluno aluno;
-	@ManyToOne
-	private Disciplina disciplina;
-	@Column(length = 2, scale = 2, nullable = true, name ="valor")
+
+	// Exclui os atributos aluno e disciplina
+	@Column(length = 2, scale = 2, nullable = true, name = "valor")
 	private double valor;
 
 	public Nota(int id, Aluno aluno, Disciplina disciplina, double valor) {
 		super();
 		this.id = id;
-		this.aluno = aluno;
-		this.disciplina = disciplina;
 		this.valor = valor;
 	}
 
@@ -41,22 +36,6 @@ public class Nota {
 		this.id = id;
 	}
 
-	public Aluno getAluno() {
-		return aluno;
-	}
-
-	public void setAluno(Aluno aluno) {
-		this.aluno = aluno;
-	}
-
-	public Disciplina getDisciplina() {
-		return disciplina;
-	}
-
-	public void setDisciplina(Disciplina disciplina) {
-		this.disciplina = disciplina;
-	}
-
 	public double getValor() {
 		return valor;
 	}
@@ -65,9 +44,5 @@ public class Nota {
 		this.valor = valor;
 	}
 
-	@Override
-	public String toString() {
-		return "Nota [id=" + id + ", aluno=" + aluno + ", disciplina=" + disciplina + ", valor=" + valor + "]";
-	}
-
+	// Adicionar o método toString
 }
