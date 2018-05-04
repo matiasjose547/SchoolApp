d9083c1aa55bc0befa8f55cfe5668704d9b303f7
didilmonteir <didilmonteiro70@gmail.com>
2018-05-04 00:48:27 -0300

    Criei a classe BoletimRepository

--- SistemaEscolar-web/src/main/java/br/edu/ifal/SistemaEscolarweb/repositorios/BoletimRepository.java
+++ SistemaEscolar-web/src/main/java/br/edu/ifal/SistemaEscolarweb/repositorios/BoletimRepository.java
@@ -1,5 +1,9 @@
 package br.edu.ifal.SistemaEscolarweb.repositorios;
 
-public interface BoletimRepository {
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import br.edu.ifal.SistemaEscolarweb.modelo.Boletim;
+
+public interface BoletimRepository extends JpaRepository<Boletim, Integer>{
 
 }
