74bd08d92250d9e8287760eaea473c17324849e7
didilmonteir <didilmonteiro70@gmail.com>
2018-05-25 11:42:07 -0300

    Adicionei na classe DisciplinaController os métodos saveDisciplina, deleteDisciplina, editDisciplina e update disciplina

--- SistemaEscolar-web/src/main/java/br/edu/ifal/SistemaEscolarWeb/Controller/DisciplinaController.java
+++ SistemaEscolar-web/src/main/java/br/edu/ifal/SistemaEscolarWeb/Controller/DisciplinaController.java
@@ -2,12 +2,18 @@ package br.edu.ifal.SistemaEscolarWeb.Controller;
 
 import java.util.List;
 
+import javax.validation.Valid;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.autoconfigure.AutoConfigureOrder;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 
 import br.edu.ifal.SistemaEscolarweb.modelo.Aluno;
 import br.edu.ifal.SistemaEscolarweb.modelo.Disciplina;
@@ -20,7 +26,7 @@ public class DisciplinaController {
 
 	@Autowired
 	DisciplinaRepository disciplinaRepository;
-	
+
 	@RequestMapping(value = "/list", method = RequestMethod.GET)
 	public String listDisciplina(ModelMap model) {
 
@@ -32,17 +38,61 @@ public class DisciplinaController {
 
 		return "disciplina/list";
 	}
-	
+
 	@RequestMapping(value = "/new", method = RequestMethod.GET)
 	public String salvar(ModelMap model) {
-		
+
 		Disciplina disciplina = new Disciplina();
 		model.addAttribute("disciplina", disciplina);
 		model.addAttribute("edit", false);
-		
+
+		return "disciplina/form";
+	}
+
+	@RequestMapping(value = { "/save" }, method = RequestMethod.POST)
+	public String saveDisciplina(@Valid @ModelAttribute Disciplina disciplina, BindingResult result, ModelMap model) {
+
+		System.out.println(disciplina);
+
+		if (result.hasErrors()) {
+			return "disciplina/form";
+		}
+
+		disciplinaRepository.saveAndFlush(disciplina);
+
+		model.addAttribute("mensagem", "Disciplina " + disciplina.getNome() + " registrado com sucesso");
+
+		return "redirect:/disciplina/list";
+	}
+
+	@RequestMapping(value = { "/delete" }, method = RequestMethod.GET)
+	public String deleteDisciplina(@RequestParam("disciplinaId") Integer id) {
+		disciplinaRepository.deleteById(id);
+		return "redirect:/disciplina/list";
+	}
+	
+
+	@RequestMapping(value = { "/edit-{id}-disciplina" }, method = RequestMethod.GET)
+	public String editDisciplina(@PathVariable("id") Integer id, ModelMap model) {
+		Disciplina disciplina = disciplinaRepository.getOne(id);
+		model.addAttribute("disciplina", disciplina);
+		model.addAttribute("edit", true);
+
 		return "disciplina/form";
 	}
 
 	
-	
+	@RequestMapping(value = { "/edit-{id}-disciplina" }, method = RequestMethod.POST)
+	public String updateDisciplina(@Valid Disciplina disciplina, BindingResult result, ModelMap model) {
+		if (result.hasErrors()) {
+			return "disciplina/form";
+		}
+
+		disciplinaRepository.saveAndFlush(disciplina);
+
+		model.addAttribute("mensagem", "Disciplina " + disciplina.getNome() + " atualizado com sucesso");
+
+		return "redirect:/disciplina/list";
+	}
+
 }
