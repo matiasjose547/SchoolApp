bab022c56406953d9c0904aed3eb62a56ec375db
didilmonteir <didilmonteiro70@gmail.com>
2018-05-25 09:24:54 -0300

    Criei a classe DisciplinaController

---
+++ SistemaEscolar-web/src/main/java/br/edu/ifal/SistemaEscolarWeb/Controller/DisciplinaController.java
@@ -0,0 +1,38 @@
+package br.edu.ifal.SistemaEscolarWeb.Controller;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.autoconfigure.AutoConfigureOrder;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+import br.edu.ifal.SistemaEscolarweb.modelo.Aluno;
+import br.edu.ifal.SistemaEscolarweb.modelo.Disciplina;
+import br.edu.ifal.SistemaEscolarweb.modelo.Professor;
+import br.edu.ifal.SistemaEscolarweb.repositorios.DisciplinaRepository;
+
+@Controller
+@RequestMapping("/disciplina")
+public class DisciplinaController {
+
+	@Autowired
+	DisciplinaRepository disciplinaRepository;
+	
+	@RequestMapping(value = "/list", method = RequestMethod.GET)
+	public String listDisciplina(ModelMap model) {
+
+		List<Disciplina> disciplinas = disciplinaRepository.findAll();
+
+		model.addAttribute("disciplinaList", disciplinas);
+
+		model.addAttribute("message", "Lista de disciplinas");
+
+		return "disciplina/list";
+	}
+
+	
+	
+}
