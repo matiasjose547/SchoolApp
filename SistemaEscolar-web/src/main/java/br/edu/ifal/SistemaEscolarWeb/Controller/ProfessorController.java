2e28d854e036b749398b499320c34d548f60e292
didilmonteir <didilmonteiro70@gmail.com>
2018-05-28 21:30:39 -0300

    Adicionei na classe ProfessorController os métodos para adicionar, deletar, editar e salvar

--- SistemaEscolar-web/src/main/java/br/edu/ifal/SistemaEscolarWeb/Controller/ProfessorController.java
+++ SistemaEscolar-web/src/main/java/br/edu/ifal/SistemaEscolarWeb/Controller/ProfessorController.java
@@ -2,12 +2,20 @@ package br.edu.ifal.SistemaEscolarWeb.Controller;
 
 import java.util.List;
 
+import javax.naming.Binding;
+import javax.validation.Valid;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 
+import br.edu.ifal.SistemaEscolarweb.modelo.Disciplina;
 import br.edu.ifal.SistemaEscolarweb.modelo.Professor;
 import br.edu.ifal.SistemaEscolarweb.repositorios.DisciplinaRepository;
 import br.edu.ifal.SistemaEscolarweb.repositorios.ProfessorRepository;
@@ -22,7 +30,7 @@ public class ProfessorController {
 	DisciplinaRepository disciplinaRepository;
 
 	@RequestMapping(value = "/list", method = RequestMethod.GET)
-	public String listAluno(ModelMap model) {
+	public String listProfessor(ModelMap model) {
 
 		List<Professor> professores = professorRepository.findAll();
 
@@ -31,5 +39,62 @@ public class ProfessorController {
 		model.addAttribute("message", "Lista de Professores");
 
 		return "professor/list";
+	}
+	
+
+	@RequestMapping(value = { "/new" }, method = RequestMethod.GET)
+	public String newProfessor(ModelMap model) {
+
+		Professor professor = new Professor();
+		model.addAttribute("professor", professor);
+		model.addAttribute("edit", false);
+
+		return "professor/form";
+	}
+	
+	@RequestMapping(value = {"/save"}, method = RequestMethod.POST)
+	public String saveProfessor(@Valid @ModelAttribute Professor professor, 
+			BindingResult result, ModelMap model) {
+
+		if (result.hasErrors()) {
+			return "professor/list";
+		}
+		
+		professorRepository.saveAndFlush(professor);
+		
+		model.addAttribute("mensagem", "Professor" + professor.getNome() + " registrado com sucesso");
+
+		return "redirect:/professor/list";
+		
+	}
+	
+	@RequestMapping(value = { "/delete" }, method = RequestMethod.GET)
+	public String deleteProfessor(@RequestParam("professorId") Integer numeroCadastro) {
+		professorRepository.deleteById(numeroCadastro);
+		return "redirect:/professor/list";
+	}
+	
+
+	@RequestMapping(value = { "/edit-{id}-professor" }, method = RequestMethod.GET)
+	public String editProfessor(@PathVariable("id") Integer id, ModelMap model) {
+		Professor professor = professorRepository.getOne(id);
+		model.addAttribute("professor", professor);
+		model.addAttribute("edit", true);
+
+		return "professor/form";
+	}
+
+	
+	@RequestMapping(value = { "/edit-{id}-professor" }, method = RequestMethod.POST)
+	public String updateProfessor(@Valid Professor professor, BindingResult result, ModelMap model) {
+		if (result.hasErrors()) {
+			return "professor/form";
+		}
+
+		professorRepository.saveAndFlush(professor);
+
+		model.addAttribute("mensagem", "Professor " + professor.getNome() + " atualizado com sucesso");
+
+		return "redirect:/professor/list";
 	}
 }
