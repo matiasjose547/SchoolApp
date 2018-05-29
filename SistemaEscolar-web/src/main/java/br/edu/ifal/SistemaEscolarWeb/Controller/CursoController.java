bddeb3b17db3462660f2af919b46198bee48c671
didilmonteir <didilmonteiro70@gmail.com>
2018-05-28 22:45:03 -0300

    Criei a classe CursoController e adicionei o método saveCurso

--- SistemaEscolar-web/src/main/java/br/edu/ifal/SistemaEscolarWeb/Controller/CursoController.java
+++ SistemaEscolar-web/src/main/java/br/edu/ifal/SistemaEscolarWeb/Controller/CursoController.java
@@ -2,9 +2,13 @@ package br.edu.ifal.SistemaEscolarWeb.Controller;
 
 import java.util.List;
 
+import javax.validation.Valid;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
@@ -27,4 +31,28 @@ public class CursoController {
 		
 	return "curso/list"; 
 	}
+	
+	public String newCurso(ModelMap model) {
+		
+		Curso curso = new Curso();
+		model.addAttribute("curso", "curso");
+		model.addAttribute("edit", false);
+		
+		return "curso/form";
+	}
+	
+	@RequestMapping(value = "/save", method = RequestMethod.POST)
+	public String saveCurso (@Valid @ModelAttribute Curso curso, BindingResult result, ModelMap model) {
+		
+		if (result.hasErrors()) {
+			return "curso/form";
+		}
+	
+		cursoRepository.saveAndFlush(curso);
+		
+		model.addAttribute("mensagem", "Curso" + curso.getNome() + " registrado com sucesso");
+		
+		return "redirect:/curso/list";
+	}
 }
+
