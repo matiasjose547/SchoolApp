3b51c6ff042a43a04285d98b06a2be20fe8a9b7d
didilmonteir <didilmonteiro70@gmail.com>
2018-05-30 01:15:09 -0300

    Adicionei na classe Curso os métodos equals e hashCode, toString e getters e setters do atributo codigo

--- SistemaEscolar-web/src/main/java/br/edu/ifal/SistemaEscolarweb/modelo/Curso.java
+++ SistemaEscolar-web/src/main/java/br/edu/ifal/SistemaEscolarweb/modelo/Curso.java
@@ -44,6 +44,15 @@ public class Curso {
 	public void setNome(String nome) {
 		this.nome = nome;
 	}
+	
+
+	public Integer getCodigo() {
+		return codigo;
+	}
+
+	public void setCodigo(Integer codigo) {
+		this.codigo = codigo;
+	}
 
 	public Set<Aluno> getAluno() {
 		return aluno;
@@ -72,4 +81,55 @@ public class Curso {
 		this.disciplina.remove(disciplina);
 	}
 
+	
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((aluno == null) ? 0 : aluno.hashCode());
+		result = prime * result + ((codigo == null) ? 0 : codigo.hashCode());
+		result = prime * result + ((disciplina == null) ? 0 : disciplina.hashCode());
+		result = prime * result + ((nome == null) ? 0 : nome.hashCode());
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Curso other = (Curso) obj;
+		if (aluno == null) {
+			if (other.aluno != null)
+				return false;
+		} else if (!aluno.equals(other.aluno))
+			return false;
+		if (codigo == null) {
+			if (other.codigo != null)
+				return false;
+		} else if (!codigo.equals(other.codigo))
+			return false;
+		if (disciplina == null) {
+			if (other.disciplina != null)
+				return false;
+		} else if (!disciplina.equals(other.disciplina))
+			return false;
+		if (nome == null) {
+			if (other.nome != null)
+				return false;
+		} else if (!nome.equals(other.nome))
+			return false;
+		return true;
+	}
+
+	@Override
+	public String toString() {
+		return "Curso [codigo=" + codigo + ", nome=" + nome + ","
+				+ " aluno=" + aluno + ", disciplina=" + disciplina + "]";
+	}
+
+	
 }
